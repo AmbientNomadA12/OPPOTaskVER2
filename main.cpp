abf6c0d6dad61b5f246747ecfe39f9a5863b65f0
AmbientNomadA12 <stalone2002@gmail.com>
2022-12-04 22:12:34 +0700

    resolved conflict 4

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 
 using namespace std;
 
+//some drastic changes
+//really bad
+//первый блин комом
 
 double my_pow(double number, int power)
 {
-<<<<<<< HEAD
 	double result = 1;
 	if (power < 0) {
 		power = -power;
@@ -23,6 +25,7 @@ double my_pow(double number, int power)
 		return result;
 	}
 }
+
 
 double my_pow_fast(double number, int power)
 {
@@ -58,41 +61,11 @@ double my_pow_fast(double number, int power)
 
 		return result;
 	}
-=======
-	int result = 1;
-	for (int i = 0; i < power; ++i)
-	{
-		result *= number;
-	}
-	return(result);
-}
-
-double my_pow_fast(double number, unsigned int power)
-{
-	long result = 1;
-	while (power) {
-		if (power % 2 == 0) {
-			power /= 2;
-			number *= number;
-		}
-		else {
-			power--;
-			result *= number;
-		}
-	}
-
-	return result;
->>>>>>> feature_fast
 }
 
 int main()
 {
-<<<<<<< HEAD
 	cout << my_pow(2, -2) << "\n";
 	cout << my_pow_fast(2, -2);
-=======
-	cout << my_pow(2, 2) << "\n";
-	cout << my_pow_fast(2, 2);
->>>>>>> feature_fast
 }
 
