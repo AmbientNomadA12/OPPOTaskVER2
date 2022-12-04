c72f2331964439534642bcce68c7cd975c11d770
AmbientNomadA12 <stalone2002@gmail.com>
2022-12-04 21:36:49 +0700

    about

--- main.cpp
+++ main.cpp
@@ -5,16 +5,34 @@ using namespace std;
 
 double my_pow(double number, unsigned int power)
 {
+	int result = 1;
 	for (int i = 0; i < power; ++i)
 	{
-		number *= number;
+		result *= number;
 	}
-	return(number);
+	return(result);
 }
 
+double my_pow_fast(double number, unsigned int power)
+{
+	long result = 1;
+	while (power) {
+		if (power % 2 == 0) {
+			power /= 2;
+			number *= number;
+		}
+		else {
+			power--;
+			result *= number;
+		}
+	}
+
+	return result;
+}
 
 int main()
 {
-	cout << my_pow(2, 2);
+	cout << my_pow(2, 2) << "\n";
+	cout << my_pow_fast(2, 2);
 }
 
