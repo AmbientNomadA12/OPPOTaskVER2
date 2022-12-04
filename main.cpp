31080d30f6d2f746e5112734efb40c2e8c52b4e3
AmbientNomadA12 <stalone2002@gmail.com>
2022-12-04 21:24:48 +0700

    about

---
+++ main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+double my_pow(double number, unsigned int power)
+{
+	for (int i = 0; i < power; ++i)
+	{
+		number *= number;
+	}
+	return(number);
+}
+
+
+int main()
+{
+	cout << my_pow(2, 2);
+}
+
